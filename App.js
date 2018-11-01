f83179664b274810edcf9e89312107854cd2ad74
Kenny <zheng323@purdue.edu>
2018-11-01 01:51:08 -0400

    testing

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import FetchLocation from './src/components/FetchLocation';
 
 export default class App extends Component<Props> {
+    getUserLocationHandler = () => {
+        console.log("pressed");
+    }
+
   render() {
     return (
       <View>
-        <Text>Hello World!</Text>
+        <FetchLocation onGetLocation={this.getUserLocationHandler} />
       </View>
     );
   }
